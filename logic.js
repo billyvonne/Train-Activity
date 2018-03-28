6104d014d81407a216435bbf7b3438b16126efa8
unknown <billy.vonne0930@gmail.com>
2018-03-28 18:28:40 -0400

    Adding initial HTML and firebase in js.

---
+++ logic.js
@@ -0,0 +1,12 @@
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAsiWsK9IfrV_ecMGPuiRfmvA3rj_z3FE8",
+        authDomain: "billyharber-bdcc3.firebaseapp.com",
+        databaseURL: "https://billyharber-bdcc3.firebaseio.com",
+        projectId: "billyharber-bdcc3",
+        storageBucket: "billyharber-bdcc3.appspot.com",
+        messagingSenderId: "525032875069"
+    };
+        firebase.initializeApp(config);
+
+        var database = firebase.database();
